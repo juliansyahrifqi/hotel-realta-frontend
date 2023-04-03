a43e04ac245af80806eab44c983e1f43cddef7cb
Rifqi Pratama <rifqipratama9@gmail.com>
2023-04-04 05:12:27 +0700

    change navbar dropdown logout

--- components/shared/Navbar/index.tsx
+++ components/shared/Navbar/index.tsx
@@ -26,80 +26,6 @@ export default function Navbar({ showSidebar, setShowSidebar }: any) {
             </a>
           </div>
 
-          {/* <div className="user-profile flex items-center gap-3">
-            <span className="font-medium">Hi, User</span>
-            <Image
-              src={Avatar}
-              width={30}
-              height={30}
-              alt="User Avatar"
-              className="rounded-full border-2 border-gray-600"
-            />
-
-            <div
-              className="z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 block"
-              id="dropdown-2"
-              style={{
-                position: "absolute",
-                inset: "0px auto auto 0px",
-                margin: "0px",
-                transform: "translate3d(184.5px, 58px, 0px)",
-              }}
-            >
-              <div className="px-4 py-3" role="none">
-                <p
-                  className="text-sm text-gray-900 dark:text-white"
-                  role="none"
-                >
-                  Neil Sims
-                </p>
-                <p
-                  className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-                  role="none"
-                >
-                  neil.sims@flowbite.com
-                </p>
-              </div>
-              <ul className="py-1" role="none">
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div> */}
           <div className="flex items-center ml-3">
             <Menu as="div" className="relative inline-block text-left">
               <div>
@@ -130,12 +56,12 @@ export default function Navbar({ showSidebar, setShowSidebar }: any) {
                 leaveFrom="transform scale-100"
                 leaveTo="transform scale-95"
               >
-                <Menu.Items className="absolute right-0 w-56 z-50 mt-2 origin-top-right bg-white rounded shadow-sm border border-variant">
+                <Menu.Items className="absolute right-0 w-56 z-50 mt-2 origin-top-right bg-white rounded shadow-sm border-2 border-primary">
                   <div className="p-1">
                     <Menu.Item>
                       <button
                         type="button"
-                        className="flex text-black hover:bg-gray-300 hover:rounded p-2 w-full text-sm group transition-colors items-center font-medium"
+                        className="flex text-primary hover:bg-primary hover:text-white hover:rounded p-2 w-full text-sm group transition-colors items-center font-medium"
                       >
                         <BiLogOut className="h-4 w-4 mr-2" />
                         Logout
