e0bacd973107b1b831667ada66f886eedcd68271
Adityanuriskandar <adityanuriskandar1@gmail.com>
2023-04-05 07:52:39 +0700

    menambahkan tampilan filter harga ASC DESC

--- pages/resto/restoMenu/index.tsx
+++ pages/resto/restoMenu/index.tsx
@@ -98,18 +98,6 @@ const restoMenu = () => {
 
   const totalPages = Math.ceil(restoMenus.length / limit)
 
-  // Pagination function
-  // const handlePageChange = (type: string) => {
-  //   if (type === 'prev' && currentPage > 1) {
-  //     setCurrentPage((prev) => prev - 1)
-  //     handleGetData() // call handleGetData to fetch data again
-  //   } else if (type === 'next' && currentPage < totalPages) {
-  //     setCurrentPage((prev) => prev + 1)
-  //     handleGetData() // call handleGetData to fetch data again
-  //   }
-  // }
-
-  // Create array of pages to display in pagination buttons
   const pageNumbers = []
   for (let i = 1; i <= totalPages; i++) {
     pageNumbers.push(i)
@@ -149,6 +137,54 @@ const restoMenu = () => {
                   onChange={handleSearchChange}
                 />
               </div>
+              {/* FILTER HIGH TO LOW AND LOW TO HIGH */}
+              {/* component */}
+              <div className='lg:ml-40 ml-10 space-x-8'>
+                <div className='relative inline-flex self-center'>
+                  <svg
+                    className='text-white bg-blue-700 absolute top-0 right-0 m-2 pointer-events-none p-2 rounded'
+                    xmlns='http://www.w3.org/2000/svg'
+                    xmlnsXlink='http://www.w3.org/1999/xlink'
+                    width='25px'
+                    height='25px'
+                    viewBox='0 0 38 22'
+                    version='1.1'
+                  >
+                    <title>F09B337F-81F6-41AC-8924-EC55BA135736</title>
+                    <g
+                      id='ZahnhelferDE—Design'
+                      stroke='none'
+                      strokeWidth={1}
+                      fill='none'
+                      fillRule='evenodd'
+                    >
+                      <g
+                        id='ZahnhelferDE–Icon&Asset-Download'
+                        transform='translate(-539.000000, -199.000000)'
+                        fill='#ffffff'
+                        fillRule='nonzero'
+                      >
+                        <g
+                          id='Icon-/-ArrowRight-Copy-2'
+                          transform='translate(538.000000, 183.521208)'
+                        >
+                          <polygon
+                            id='Path-Copy'
+                            transform='translate(20.000000, 18.384776) rotate(135.000000) translate(-20.000000, -18.384776) '
+                            points='33 5.38477631 33 31.3847763 29 31.3847763 28.999 9.38379168 7 9.38477631 7 5.38477631'
+                          />
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                  <select className='text-2xl font-bold rounded border-2 border-blue-700 text-gray-600 h-10 w-60 pl-5 pr-5 bg-white hover:border-gray-400 focus:outline-none appearance-none'>
+                    <option>Low-High</option>
+                    <option>High-Low</option>
+                  </select>
+                </div>
+              </div>
+              {/* </> */}
+
               <div className='lg:ml-40 ml-10 space-x-8'>
                 {/* <button className='bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer'>
                   New Report
@@ -242,10 +278,10 @@ const restoMenu = () => {
                             <td className='px-6 py-3 text-sm text-gray-900'>
                               <Menu
                                 as='div'
-                                className='relative inline-block text-left'
+                                className='relative inline-block  text-left'
                               >
                                 <div>
-                                  <Menu.Button className='inline-flex w-full justify-center rounded-md bg-none px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
+                                  <Menu.Button className='inline-flex  w-full justify-center rounded-md bg-none px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
                                     <BsThreeDotsVertical
                                       className='ml-2 -mr-1 h-5 w-5 text-blue-900 hover:text-violet-300'
                                       aria-hidden='true'
@@ -261,7 +297,7 @@ const restoMenu = () => {
                                   leaveFrom='transform opacity-100 scale-100'
                                   leaveTo='transform opacity-0 scale-95'
                                 >
-                                  <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10'>
+                                  <Menu.Items className='absolute  right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10'>
                                     {/* EDIT */}
                                     <div className='px-1 py-1 '>
                                       <Menu.Item>
@@ -269,7 +305,7 @@ const restoMenu = () => {
                                           <button
                                             className={`${
                                               active
-                                                ? 'bg-violet-500 text-white'
+                                                ? 'bg-blue-500 text-white'
                                                 : 'text-gray-900'
                                             } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                                             onClick={() =>
@@ -302,7 +338,7 @@ const restoMenu = () => {
                                           <button
                                             className={`${
                                               active
-                                                ? 'bg-violet-500 text-white'
+                                                ? 'bg-blue-500 text-white'
                                                 : 'text-gray-900'
                                             } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                                             onClick={() =>
@@ -332,7 +368,7 @@ const restoMenu = () => {
                                           <button
                                             className={`${
                                               active
-                                                ? 'bg-violet-500 text-white'
+                                                ? 'bg-blue-500 text-white'
                                                 : 'text-gray-900'
                                             } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                                             onClick={() =>
