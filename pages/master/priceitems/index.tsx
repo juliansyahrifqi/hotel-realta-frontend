8e2317e956351bd9ea5d6ee0ad0529559aeed869
Rifqi Pratama <rifqipratama9@gmail.com>
2023-04-15 19:55:19 +0700

    change ikrar priceitems

--- pages/master/priceitems/index.tsx
+++ pages/master/priceitems/index.tsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Modal from "./componentmodal";
 import Button from "@/components/Button/button";
 import AddPriceMaster from "./addPriceItems";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdEdit, MdDelete } from "react-icons/md";
 
 export default function PriceMaster() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -25,6 +27,7 @@ export default function PriceMaster() {
     { name: "Description" },
     { name: "Price" },
     { name: "Type" },
+    { name: "Action" },
   ];
 
   // console.log("data", priceitems);
@@ -33,75 +36,88 @@ export default function PriceMaster() {
     dispatch(doRequestGetPriceItems(searchQuery, searchType, page, limit));
   }, [limit, page, dispatch, refresh, searchQuery, searchType]);
   return (
-    <div className="relative overflow-y-auto  shadow-md sm:rounded-lg">
+    <div className="relative overflow-x-auto shadow-md mt-5 rounded-xl bg-white p-4">
       {/* search */}
       <div>
-        <form className="flex items-center">
-          <div className="relative w-10"></div>
-          {/* batas atas search name */}
-          <label htmlFor="simple-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative w-80">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+        <form className="pb-4">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <div>
+                <label htmlFor="simple-search" className="sr-only">
+                  Search
+                </label>
+                <div className="relative w-80">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                  <input
+                    type="text"
+                    id="simple-search"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Employee Name"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="status" className="sr-only">
+                  Status
+                </label>
+                <div className="relative w-44 ml-2">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                  <select
+                    id="status"
+                    name="status"
+                    className="block w-full py-2.5 px-8 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  >
+                    <option selected>Type</option>
+                    <option value="SNACK">SNACK</option>
+                    <option value="FACILITY">FACILITY</option>
+                    <option value="FOOD">FOOD</option>
+                    <option value="SOFTDRINK">SOFTDRINK</option>
+                    <option value="SERVICE">SERVICE</option>
+                  </select>
+                </div>
+              </div>
             </div>
-            <input
-              type="text"
-              id="simple-search"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Employee Name"
-              required
+
+            <Button
+              variant="primary"
+              label="Add"
+              size="small"
+              type="secondary"
+              className="ml-0"
+              onClick={() => setIsOpen(true)}
+              icon={AiOutlinePlus}
             />
           </div>
-          {/* batas bawah search name */}
-
-          {/* batas atas search status */}
-          <label htmlFor="status" className="sr-only">
-            Status
-          </label>
-          <div className="relative w-44 ml-2">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <select
-              id="status"
-              name="status"
-              className="block w-full py-2.5 px-8 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            >
-              <option selected>Type</option>
-              <option value="SNACK">SNACK</option>
-              <option value="FACILITY">FACILITY</option>
-              <option value="FOOD">FOOD</option>
-              <option value="SOFTDRINK">SOFTDRINK</option>
-              <option value="SERVICE">SERVICE</option>
-            </select>
-          </div>
-          {/* batas bawah search status */}
         </form>
       </div>
 
@@ -126,16 +142,6 @@ export default function PriceMaster() {
                 </td>
               </>
             ))}
-            <td className="py-2 flex pl-6 border-black bg-gray-50 text-left text-xs font-medium text-black uppercase tracking-wider ">
-              <Button
-                variant="variant"
-                label="Add"
-                size="small"
-                type="secondary"
-                className="ml-0"
-                onClick={() => setIsOpen(true)}
-              />
-            </td>
           </tr>
         </thead>
         <tbody>
@@ -163,24 +169,27 @@ export default function PriceMaster() {
               <td className="flex items-center px-6 py-4 space-x-3">
                 <a
                   href="#"
-                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  className="border-2 border-primary hover:bg-primary hover:text-white transition-colors ease-in duration-100 p-2 rounded text-primary"
                   // onClick={() => editOpen(dt.poli_id)}
                 >
-                  Edit
+                  <MdEdit className="text-xl" />
                 </a>
                 <a
                   href="#"
-                  className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                  className="border-2 border-danger-secondary hover:bg-danger-secondary hover:text-white transition-colors ease-in duration-100 p-2 rounded text-danger-secondary"
                   // onClick={() => deleteOpen(dt.poli_id)}
                 >
-                  Remove
+                  <MdDelete className="text-xl" />
                 </a>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <nav aria-label="Page navigation example">
+      <nav
+        aria-label="Page navigation example"
+        className="flex items-center justify-end pt-4"
+      >
         <ul className="inline-flex items-center -space-x-px">
           <li>
             <a
